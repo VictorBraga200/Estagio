3d8356908e60152f9473c4243f2cee64391df8f5
Victor Braga <vitimeeu@gmail.com>
2021-03-03 23:27:54 -0300

    Versao Final

--- TestePadwansAlbuns.php
+++ TestePadwansAlbuns.php
@@ -1,70 +1,71 @@
 <!DOCTYPE html>
-
-<title>Albuns</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="css/estilos.css">
-
 <html>
-    <header> ​
-        <!-- Navbar -->
-        <div class="w3-top">
-            <div class="w3-bar w3-purple w3-card">
-                <a href="Principal.html" class="w3-bar-item w3-button w3-padding-large">HOME</a>
-                <a href="TestePadwansPost.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">POSTAGENS</a>
-                <a href="TestePadwansAlbuns.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">ÁLBUNS</a>
-                <a href="TestePadwansTodos.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">TODOS</a>
-            </div>
-            ​
-        </div>
-    </header>
+    <head>
+    <title>Albuns</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="img/icon.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/estilos.css">
+    </head>
 
     <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#" title="Visite meu likedln">Victor Braga</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="TestePadwansPost.php">Postagens</a></li>
+                    <li class="active"><a href="TestePadwansAlbuns.php">Álbuns</a></li>
+                    <li><a href="TestePadwansTodos.php">Todos</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
 
-        <?php
-        $sample_data = file_get_contents("https://jsonplaceholder.typicode.com/albums");
 
-        // just normal getting data
-        $raw_data = json_decode($sample_data, true);
-        //$raw_data = $raw_data['qqq'];
+            <?php
+            $sample_data = file_get_contents("https://jsonplaceholder.typicode.com/albums");
 
-        // use get variable to paging number
-        $page = !isset($_GET['page']) ? 1 : $_GET['page'];
-        $limit = 10; // five rows per page
-        $offset = ($page - 1) * $limit; // offset
-        $total_items = count($raw_data); // total items
-        $total_pages = ceil($total_items / $limit);
-        $final = array_splice($raw_data, $offset, $limit); // splice them according to offset and limit
+            // just normal getting data
+            $raw_data = json_decode($sample_data, true);
+            //$raw_data = $raw_data['qqq'];
 
-        ?>
-        <table border="1" cellpadding="10">
-            <tr>
-                <th>Codigo usuario</th>
-                <th>Id</th>
-                <th>Titulo</th>
-            </tr>
-            <?php foreach ($final as $key => $value) : ?>
+            // use get variable to paging number
+            $page = !isset($_GET['page']) ? 1 : $_GET['page'];
+            $limit = 10; // Limite de dados por pagina
+            $offset = ($page - 1) * $limit; 
+            $total_items = count($raw_data); // total items
+            $total_pages = ceil($total_items / $limit);
+            $final = array_splice($raw_data, $offset, $limit); // splice them according to offset and limit
+
+            ?>
+            <table class="table table-hover">
                 <tr>
-                    <?php foreach ($value as $index => $element) : ?>
-                        <td><?php echo !is_array($element) ? $element : implode(',', $element); ?></td>
-                    <?php endforeach; ?>
+                    <th>Codigo usuario</th>
+                    <th>Id</th>
+                    <th>Titulo</th>
                 </tr>
-            <?php endforeach; ?>
-        </table>
+                <?php foreach ($final as $key => $value) : ?>
+                    <tr>
+                        <?php foreach ($value as $index => $element) : ?>
+                            <td><?php echo !is_array($element) ? $element : implode(',', $element); ?></td>
+                        <?php endforeach; ?>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
 
-        <!-- print links -->
-        <?php for ($x = 1; $x <= $total_pages; $x++) : ?>
-            <a href='TestePadwansAlbuns.php?page=<?php echo $x; ?>'><?php echo $x; ?></a>
-        <?php endfor; ?>
+            <!-- Paginacao -->
+            <?php for ($x = 1; $x <= $total_pages; $x++) : ?>
+                <ul class="pagination">
+                    <li <?php if($page==$x){echo "class='page-item active'";}else{echo "class='page-item'";}?>><a class="page-link" href='TestePadwansAlbuns.php?page=<?php echo $x; ?>'><?php echo $x; ?></a></li>
+                </ul>
+            <?php endfor; ?>
 
-        <footer class="container-fluid text-center bg-grey">
-        <a href="TestePadwansTodos.php" title="To Top">
-            <span class="glyphicon glyphicon-chevron-up"></span>
-        </a>
-        <p>Feito por <a href="https://www.linkedin.com/in/victor-hugo-156953114/" title="Visite o likedln">Victor Braga</a></p>
-    </footer>
+        </div>
 
     </body>
 
--- TestePadwansTodos.php
+++ TestePadwansTodos.php
@@ -1,72 +1,91 @@
 <!DOCTYPE html>
 <html>
-<title>Todos</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="css/estilos.css">
 
-<header> ​
-    <!-- Navbar -->
-    <div class="w3-top">
-        <div class="w3-bar w3-purple w3-card">
-            <a href="Principal.html" class="w3-bar-item w3-button w3-padding-large">HOME</a>
-            <a href="TestePadwansPost.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">POSTAGENS</a>
-            <a href="TestePadwansAlbuns.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">ÁLBUNS</a>
-            <a href="TestePadwansTodos.php" class="w3-bar-item w3-button w3-padding-large w3-hide-small">TODOS</a>
-        </div>
-        ​
-    </div>
-</header>
+    <head>
+        <title>Todos</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="img/icon.png">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="https://kryogenix.org/code/browser/sorttable/sorttable.js"></script>
+        <link rel="stylesheet" href="css/estilos.css">
+    </head>
 
-<body>
+    <body>
 
-    <?php
-    $sample_data = file_get_contents("https://jsonplaceholder.typicode.com/todos");
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#" title="Visite meu likedln">Victor Braga</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="TestePadwansPost.php">Postagens</a></li>
+                    <li><a href="TestePadwansAlbuns.php">Álbuns</a></li>
+                    <li class="active"><a href="TestePadwansTodos.php">Todos</a></li>
+                </ul>
+            </div>
+        </nav>
 
-    // just normal getting data
-    $raw_data = json_decode($sample_data, true);
-    //$raw_data = $raw_data['qqq'];
+        <div class="container">
 
-    // use get variable to paging number
-    $page = !isset($_GET['page']) ? 1 : $_GET['page'];
-    $limit = 10; // five rows per page
-    $offset = ($page - 1) * $limit; // offset
-    $total_items = count($raw_data); // total items
-    $total_pages = ceil($total_items / $limit);
-    $final = array_splice($raw_data, $offset, $limit); // splice them according to offset and limit
+            <?php
+            $sample_data = file_get_contents("https://jsonplaceholder.typicode.com/todos");
 
-    ?>
-    <table cellpadding="10">
-        <tr>
-            <th>Codigo usuario</th>
-            <th>Id</th>
-            <th>Titulo</th>
-            <th>Completo</th>
-        </tr>
-        <?php foreach ($final as $key => $value) : ?>
-            <tr>
-                <?php foreach ($value as $index => $element) : ?>
-                    <td><?php echo !is_array($element) ? $element : implode(',', $element); ?></td>
+            // just normal getting data
+            $raw_data = json_decode($sample_data, true);
+            // var_dump($raw_data);
+            //$raw_data = $raw_data['qqq'];
+
+            // use get variable to paging number
+            $page = !isset($_GET['page']) ? 1 : $_GET['page'];
+            $limit = 10; // five rows per page
+            $offset = ($page - 1) * $limit; // offset
+            $total_items = count($raw_data); // total items
+            $total_pages = ceil($total_items / $limit);
+            $final = array_splice($raw_data, $offset, $limit); // splice them according to offset and limit
+
+            ?>
+            <table class="table table-hover sortable">
+                <tr>
+                    <th>Codigo usuario</th>
+                    <th>Id</th>
+                    <th>Titulo</th>
+                    <th>Completo</th>
+                </tr>
+                <?php foreach ($final as $key => $value) : ?>
+                    <tr>
+                        <?php foreach ($value as $index => $element) : ?>
+                            <?php if ($index == 'completed') {
+                                if ($element) {
+                                    $completed = 'True';
+                                } else {
+                                    $completed = 'False';
+                                }
+                                echo "<td>" . $completed . "</td>";
+                            } else {
+                                echo "<td>" . $element . "</td>";
+                            }
+                            ?>
+
+                        <?php endforeach; ?>
+                    </tr>
                 <?php endforeach; ?>
-            </tr>
-        <?php endforeach; ?>
-    </table>
+            </table>
 
-    <!-- print links -->
-    <?php for ($x = 1; $x <= $total_pages; $x++) : ?>
-        <a href='TestePadwans.php?page=<?php echo $x; ?>'><?php echo $x; ?></a>
-    <?php endfor; ?>
+            <!-- print links -->
+            <?php for ($x = 1; $x <= $total_pages; $x++) : ?>
+                <ul class="pagination">
+                    <li <?php if ($page == $x) {
+                            echo "class='page-item active'";
+                        } else {
+                            echo "class='page-item'";
+                        } ?>><a class="page-item" href='TestePadwansTodos.php?page=<?php echo $x; ?>'><?php echo $x; ?></a></li>
+                </ul>
+            <?php endfor; ?>
+        </div>
 
-    <footer class="container-fluid text-center bg-grey">
-        <a href="#myPage" title="To Top">
-            <span class="glyphicon glyphicon-chevron-up"></span>
-        </a>
-        <p>Feito por <a href="https://www.linkedin.com/in/victor-hugo-156953114/" title="Visite o likedln">Victor Braga</a></p>
-    </footer>
-
-</body>
+    </body>
 
 </html>
